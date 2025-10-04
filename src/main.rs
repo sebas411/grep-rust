6bd173edc939647dc30cefac0d5119e76649eb26
smaldonado411 <smaldonado@distelsa.com.gt>
2025-10-04 14:34:26 -0600

    Added compatibility for multiline files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 use std::fs::File;
-use std::io;
-use std::io::Read;
+use std::io::{self, BufRead, BufReader};
 use std::process;
 
 
@@ -389,11 +388,14 @@ fn main() {
 
     if files_to_search.len() > 0 {
         for filename in files_to_search {
-            let mut file = File::open(&filename).expect("File should be readable");
-            file.read_to_string(&mut input_line).expect("It should be readable to string");
-            if matchgen(&pattern, &input_line) {
-                found_match = true;
-                println!("{}", input_line);
+            let file = File::open(&filename).expect("File should be readable");
+            let reader = BufReader::new(file);
+            for line in reader.lines() {
+                input_line = line.expect("File should split into lines");
+                if matchgen(&pattern, &input_line) {
+                    found_match = true;
+                    println!("{}", input_line);
+                }
             }
         }
     } else {
